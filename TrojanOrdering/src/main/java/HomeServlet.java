3c65e107f15cf4fce1ea5f5f8d4d9a7ea249ff69
Mitch Pi <mitchpi2000@gmail.com>
2023-04-22 00:59:54 -0700

    Returns response to home.html with formatted rows of restaurants

--- TrojanOrdering/src/main/java/HomeServlet.java
+++ TrojanOrdering/src/main/java/HomeServlet.java
@@ -16,20 +16,36 @@ public class HomeServlet extends HttpServlet {
             throws ServletException, IOException {
     	PrintWriter out = response.getWriter();
         ArrayList<Restaurant> r = JDBCConnector.getRestaurants();
+        for (Restaurant rest : r){ //placeholder stuff please give me a format to work with
+            System.out.println(rest.getName());
+        } 
         String html = makeHtml(r);
         //return html to javascript using fetch 
         response.setContentType("text/html");
         out.println(html);
+        out.flush();
+        out.close();
 
     }
 
-    protected String makeHtml(ArrayList<Restaurant> r){
+    protected String makeHtml(ArrayList<Restaurant> r) {
         String html = "";
-        for (Restaurant rest : r){ //placeholder stuff please give me a format to work with
+        int count = 0;
+        for (Restaurant rest : r) {
             String name = rest.getName();
             String address = rest.getAddress();
             String image = rest.getURL();
-            html += "//give me something here";
+            if (count % 2 == 0) { // start a new row
+                html += "<div class = \"row\">\n";
+            }
+            html += "<a class = \"container\" href = \"restaurants.html...\"> <!--redirect to restaurants page for specified restaurant-->\n"
+                + "<img src = \""+image+"\">\n"
+                + "<div class = \"caption\">"+name+"</div>\n"
+                + "</a>\n";
+            if (count % 2 != 0 || count == r.size() - 1) { // close the row if it's the second restaurant in the row or if it's the last restaurant in the list
+                html += "</div>\n";
+            }
+            count++;
         }
         return html;
     }
