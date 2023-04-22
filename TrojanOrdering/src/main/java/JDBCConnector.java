25d6173a5ab13e50c2197aeefeba4e550bf6f088
Mitch Pi <mitchpi2000@gmail.com>
2023-04-21 17:41:49 -0700

    removed the second class declaration that I put by mistake

--- TrojanOrdering/src/main/java/JDBCConnector.java
+++ TrojanOrdering/src/main/java/JDBCConnector.java
@@ -1,10 +1,9 @@
-
-public class JDBCConnector {
-	import java.sql.Connection;
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 	public class JDBCConnector {
 	        
 	    
