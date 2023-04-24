2993253fb8cbab3192715fc7712ca4acc16fbd34
Mitch Pi <mitchpi2000@gmail.com>
2023-04-23 23:43:37 -0700

    add Chetan's registerUser,loginUser,getBalance functions

--- TrojanOrdering/src/main/java/JDBCConnector.java
+++ TrojanOrdering/src/main/java/JDBCConnector.java
@@ -11,18 +11,6 @@ import java.util.ArrayList;
 	    	pstmt.close();
 		}
 		
-		public int getBalance(int user_id) throws SQLException {
-			int result = -1;
-			Connection conn = DriverManager.getConnection("jdbc:mysql://placeholder", "root", "password");
-			PreparedStatement ps = conn.prepareStatement("SELECT balance from users WHERE userID=?");
-			ps.setInt(1, user_id);
-
-		    ResultSet rs = ps.executeQuery();
-		    if (rs.next()) {
-		        result = rs.getInt("balance");
-		    }
-			return result;
-		}
 		
 		public boolean updateBalance(int user_id, int balance, int total) throws SQLException {
 			balance = balance - total;
@@ -217,5 +205,115 @@ import java.util.ArrayList;
 		    }
 			return restaurants;
 
+		}
+		public int registerUser(String username, String password, String email, double balance) throws SQLException {
+		    int userID = -3;
+		    try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+			} catch (ClassNotFoundException e1) {
+				e1.printStackTrace();
+			}
+
+		    Connection conn  =null;
+		    try{
+		    	conn = DriverManager.getConnection("");
+		        String query = "SELECT * FROM User WHERE email=?";
+		        try (PreparedStatement ps = conn.prepareStatement(query)) {
+		            ps.setString(1, email);
+		            try (ResultSet rs = ps.executeQuery()) {
+		                if (rs.next()) {
+		                    return -1; // email already exists
+		                }
+		            }
+		        }
+		        query = "SELECT * FROM User WHERE username=?";
+		        try (PreparedStatement ps = conn.prepareStatement(query)) {
+		            ps.setString(1, username);
+		            try (ResultSet rs = ps.executeQuery()) {
+		                if (rs.next()) {
+		                    return -2; 
+		                }
+		            }
+		        }
+		        query = "INSERT INTO User (username, password, email, balance) VALUES (?, ?, ?, ?)";
+		        try (PreparedStatement ps = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+		            ps.setString(1, username);
+		            ps.setString(2, password);
+		            ps.setString(3, email);
+		            ps.setDouble(4, balance);
+		            int rowsAffected = ps.executeUpdate();
+		            if (rowsAffected == 1) {
+		                try (ResultSet rs = ps.getGeneratedKeys()) {
+		                    if (rs.next()) {
+		                        userID = rs.getInt(1);
+		                    }
+		                }
+		            }
+		        }
+		    }catch (SQLException e) {
+		        e.printStackTrace();
+		    }
+
+		    return userID;
+		}
+
+
+		public int loginUser(String username, String password) throws SQLException {
+		    int userID = -1;
+		    try {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+				} catch (ClassNotFoundException e1) {
+					e1.printStackTrace();
+				}
+		    try (Connection conn = DriverManager.getConnection("")) {
+		        String query = "SELECT * FROM User WHERE username=?";
+		        try(PreparedStatement ps = conn.prepareStatement(query)){
+			        ps.setString(1, username);
+			        try(ResultSet rs = ps.executeQuery()){
+				        if (!rs.next()) {
+				            rs.close();
+				            ps.close();
+				            return -1;
+				        } 
+				        else {
+				            String dbPassword = rs.getString("password");
+				            userID = rs.getInt("user_id");
+				            if (!dbPassword.equals(password)) {
+				                rs.close();
+				                ps.close();
+				                return -2;
+				            }
+				        }
+				        rs.close();
+				        ps.close();
+			        }
+		        }
+		    }catch (SQLException e) {
+		        e.printStackTrace();
+		    } 
+		    return userID;
+		}
+
+		public double getBalance(int userID) {
+		    try {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+				} catch (ClassNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+		    double balance = -1;
+		    String query = "SELECT balance FROM User WHERE user_id=?";
+		    try (Connection conn = DriverManager.getConnection("");
+		    		PreparedStatement ps = conn.prepareStatement(query)) {
+		    			ps.setInt(1, userID);
+		    			try(ResultSet rs = ps.executeQuery()){
+	    					if (rs.next()) {
+	    						balance = rs.getDouble("balance");
+	    					}
+		    			}
+		    } catch (SQLException e) {
+		        e.printStackTrace();
+		    }
+		    return balance;
 		}
 }
